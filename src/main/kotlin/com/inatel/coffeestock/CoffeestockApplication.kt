6e30c37839748dfb10ae6f4ec7fe01c04fb438a0
João Delfino <joaohsdelfino@gmail.com>
2023-11-04 09:12:26 -0300

    Starting application with desired dependencies (Spring Web, etc).

---
+++ src/main/kotlin/com/inatel/coffeestock/CoffeestockApplication.kt
@@ -0,0 +1,11 @@
+package com.inatel.coffeestock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoffeestockApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoffeestockApplication>(*args)
+}
---
+++ src/test/kotlin/com/inatel/coffeestock/CoffeestockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.inatel.coffeestock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoffeestockApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
